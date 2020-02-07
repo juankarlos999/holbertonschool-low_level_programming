83532fcf4998bbc8e77f533673c44be440c6b93f
Juank <juan@pop-os.localdomain>
2020-02-06 23:57:20 -0500

    program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 48; num1 <= 55; num1++)
+	{
+		for (num2 = 49; num2 <= 56; num2++)
+		{
+			for (num3 = 50; num3 <= 57; num3++)
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					if (num1 == 55 && num2 == 56 && num3 == 57)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(num3);
+					}
+					else
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(num3);
+						putchar(44);
+						putchar(32);
+					}
+				}
+				
+			}
+		}
+	}
+	putchar('\n');
+
+return (0);
+}
