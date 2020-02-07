20f608ac543efcf6a394a170ae7336db03c5996f
Juank <juan@pop-os.localdomain>
2020-02-06 20:32:09 -0500

    prints numbers from 0 to 9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Funtion that prints the numbers from 0 to 9
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+return (0);
+}
