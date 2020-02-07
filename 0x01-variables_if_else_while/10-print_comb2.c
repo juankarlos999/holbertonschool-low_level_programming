7bafddde228aaf7f703f038b426a518a92da083b
Juank <juan@pop-os.localdomain>
2020-02-06 21:41:01 -0500

    program that prints the numbers from 00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -2,13 +2,15 @@
 /**
  * main - Funtion that prints the numbers from 0 to 99
  *
- * Return: (0)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	for (int num1 = 48; num1 < 58; num1++)
+	int num1, num2;
+
+	for (num1 = 48; num1 < 58; num1++)
 	{
-		for (int num2 = 48; num2 < 58; num2++)
+		for (num2 = 48; num2 < 58; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
