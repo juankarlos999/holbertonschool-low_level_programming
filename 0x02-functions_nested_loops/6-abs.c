c9dae552a6812e3fe9e8477e57ee39487dac3622
Juank <juan@pop-os.localdomain>
2020-02-11 10:13:13 -0500

    unction that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,9 @@
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @i: Number positive or negative
+ * Return: absolute value of a integer
+ **/
+int _abs(int i)
+{
+	return (i < 0 ? ((i) * -1) : (i));
+}
