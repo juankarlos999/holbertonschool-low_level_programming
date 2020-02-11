261546accc024071ce5d5de363cbbb8d23bcdcb0
Juank <juan@pop-os.localdomain>
2020-02-11 16:58:30 -0500

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - unction that adds two integers and returns the result
+ * @a: number 1
+ * @b: number 2
+ * Return: adds two integers
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
