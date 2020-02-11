27db92fae8d4ae8749cdd6b68a0e1ef52be610bc
Juank <juan@pop-os.localdomain>
2020-02-11 00:14:36 -0500

    function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: character in value ASCII
+ * Return: (1) if c is a letter, lowercase or uppercase or (0) otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+return (0);
+}
