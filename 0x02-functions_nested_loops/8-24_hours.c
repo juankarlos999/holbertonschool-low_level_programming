e4e4d898364f5562e8ed259d2f8988572fd51d78
Juank <juan@pop-os.localdomain>
2020-02-11 12:02:53 -0500

    function that prints every minute starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * jack_bauer - function that prints every minute starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour, hour1, min, min1;
+
+	for (hour = '0'; hour <= '2'; hour++)
+	{
+		for (hour1 = '0'; hour1 <= '3'; hour1++)
+		{
+			for (min = '0'; min <= '5'; min++)
+			{
+				for (min1 = '0'; min1 <= '9'; min1++)
+				{
+					_putchar(hour);
+					_putchar(hour1);
+					_putchar(':');
+					_putchar(min);
+					_putchar(min1);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
