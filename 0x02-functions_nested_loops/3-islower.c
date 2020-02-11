3ec1bfd05aa92653dee456dcd6645dc6b653a945
Juank <juan@pop-os.localdomain>
2020-02-11 00:25:52 -0500

    function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 /**
  * _islower - function that checks for lowercase character
- * @c: character
+ * @c: variable in ASCII code
  * Return: (1) if c is lowercase or (0) otherwise
  */
 int _islower(int c)
