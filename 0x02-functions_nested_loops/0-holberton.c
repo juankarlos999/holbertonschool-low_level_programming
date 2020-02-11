bd153be5b08c8b86d7c9260dd1fe59d149c2298e
Juank <juan@pop-os.localdomain>
2020-02-11 00:11:41 -0500

    program that prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - program that prints Holberton, followed by a new line with _putchar
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+	char word[9] = "Holberton";
+
+	for (i = 0; i < word[i]; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+return (0);
+}
+
