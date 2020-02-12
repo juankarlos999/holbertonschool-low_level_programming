93811480581ce6a3a4381e243cd0eadd58466330
Juank <juan@pop-os.localdomain>
2020-02-11 22:51:19 -0500

    main file to test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+i#include "holberton.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	num = positive_or_negative(i);
+
+	return (0);
+}
