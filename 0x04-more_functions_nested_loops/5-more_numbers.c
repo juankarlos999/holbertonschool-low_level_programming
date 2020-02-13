5dd7e859c96490ef045f320d21544f82c3076af1
Juank <juan@pop-os.localdomain>
2020-02-13 12:42:13 -0500

    Function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
 void more_numbers(void)
 {
 	short i, j;
+	short range = 14;
+	short times = 10;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < times; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= range; j++)
 		{
 			if (j > 9)
 			{
 				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
 			}
-			else
-				_putchar('0' + j);
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
