6d5b9ebdea4b593d2f07487930412c3bb26be9da
Juank <juan@pop-os.localdomain>
2020-02-13 14:43:29 -0500

    function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+/**
+ * _isdigit - Function that checks for a digit (0 through 9)
+ * @c: A digit
+ * Return: 1 if c is a digit - 0 otherwise
+ **/
+int _isdigit(int c)
+{
+	return (c <= '0'? 1 : 0);
+}
