b6cf6219f4940df9b9d0d13c7f26e66411a531c6
Juank <juan@pop-os.localdomain>
2020-02-14 22:40:01 -0500

    function that prints any integer MIN-MAX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * print_number - function that prints an integer
- * @:n integer parameter
+ * print_number - function that prints any integer positive or negative
+ * @:n integer positive or negative
  * @int:integer
  **/
 void print_number_4_digits(int);
 void print_number_3_digits(int);
 void print_number_2_digits(int);
+void print_number_9_digits(int);
 void print_number_max_digits(unsigned int);
 void print_number(int n)
 {
@@ -28,17 +29,22 @@ void print_number(int n)
 			_putchar('-');
 			print_number_2_digits(n);
 		}
-		else if (n > 99 && n < 1000)
+		else if (n > 99 && n < 1e3)
 		{
 			_putchar('-');
 			print_number_3_digits(n);
 		}
-		else if (n > 999 && n < 10000)
+		else if (n > 999 && n < 1e4)
 		{
 			_putchar('-');
 			print_number_4_digits(n);
 		}
-		else if (aux > 999999999 || aux <= 2147483648)
+		else if (n > 99999999 && n < 1e9)
+		{
+			_putchar('-');
+			print_number_9_digits(n);
+		}
+		else if (aux > 999999999 && aux <= 2147483648)
 		{
 			_putchar('-');
 			print_number_max_digits(aux);
@@ -48,22 +54,26 @@ void print_number(int n)
 	{
 		print_number_2_digits(n);
 	}
-	else if (n > 99 && n < 1000)
+	else if (n > 99 && n < 1e3)
 	{
 		print_number_3_digits(n);
 	}
-	else if (n > 999 && n < 10000)
+	else if (n > 999 && n < 1e4)
 	{
 		print_number_4_digits(n);
 	}
-	else if (n > 999999999 || n <= 2147483647)
+	else if (n > 99999999 && n < 1e9)
+	{
+		print_number_9_digits(n);
+	}
+	else if (n > 999999999 && n <= 2147483647)
 	{
 		print_number_max_digits(n);
 	}
 }
 /**
- * print_number_4_digits - process a number for print
- * @number: integer for print
+ * print_number_4_digits - receives a number for slice and print
+ * @number: integer positive or negative
  */
 void print_number_4_digits(int number)
 {
@@ -79,8 +89,8 @@ void print_number_4_digits(int number)
 	_putchar('0' + (number % 10));
 }
 /**
- * print_number_3_digits - process a number for print
- * @number: integer for print
+ * print_number_3_digits - receives a number for slice and print
+ * @number: integer positive or negative
  */
 void print_number_3_digits(int number)
 {
@@ -92,8 +102,8 @@ void print_number_3_digits(int number)
 	_putchar('0' + (number % 10));
 }
 /**
- * print_number_2_digits - process a number for print
- * @number: integer for print
+ * print_number_2_digits - receives a number for slice and print
+ * @number: integer positive or negative
  */
 void print_number_2_digits(int number)
 {
@@ -101,36 +111,57 @@ void print_number_2_digits(int number)
 	_putchar('0' + (number % 10));
 }
 /**
- * print_number_max_digits - process a number for print
- * @n: integer for print
+ * print_number_max_digits - receives a number for slice and print
+ * @n: integer positive or negative
  */
 void print_number_max_digits(unsigned int n)
 {
-	unsigned int aux1 = 0;
-	unsigned int aux2 = 0;
-	unsigned int aux3 = 0;
-	unsigned int aux4 = 0;
-	unsigned int aux5 = 0;
-	unsigned int aux6 = 0;
-	unsigned int aux7 = 0;
+	unsigned int aux1, aux2, aux3, aux4 = 0;
+	unsigned int aux5, aux6, aux7 = 0;
 
-		aux1 = n / 100000000;
+		aux1 = n / 1e8;
 		_putchar('0' + (aux1 / 10));
 		aux2 = n % 1000000000;
-		aux2 = aux2 / 10000000;
+		aux2 = aux2 / 1e7;
 		_putchar('0' + (aux2 / 10));
 		aux3 = n % 100000000;
-		aux3 = aux3 / 1000000;
+		aux3 = aux3 / 1e6;
 		_putchar('0' + (aux3 / 10));
 		aux4 = n % 10000000;
-		aux4 = aux4 / 100000;
+		aux4 = aux4 / 1e5;
 		_putchar('0' + (aux4 / 10));
 		aux5 = n % 1000000;
-		aux5 = aux5 / 10000;
+		aux5 = aux5 / 1e4;
 		_putchar('0' + (aux5 / 10));
 		aux6 = n % 100000;
-		aux6 = aux6 / 1000;
+		aux6 = aux6 / 1e3;
 		_putchar('0' + (aux6 / 10));
 		aux7 = n % 10000;
 		print_number_4_digits(aux7);
+}
+/**
+ * print_number_9_digits - receives a number for slice and print
+ * @n: integer positive or negative
+ **/
+void print_number_9_digits(int n)
+{
+	int aux1, aux2, aux3, aux4, aux5, aux6 = 0;
+
+		aux1 = n % 1000000000;
+		aux1 = aux1 / 1e7;
+		_putchar('0' + (aux1 / 10));
+		aux2 = n % 100000000;
+		aux2 = aux2 / 1e6;
+		_putchar('0' + (aux2 / 10));
+		aux3 = n % 10000000;
+		aux3 = aux3 / 1e5;
+		_putchar('0' + (aux3 / 10));
+		aux4 = n % 1000000;
+		aux4 = aux4 / 1e4;
+		_putchar('0' + (aux4 / 10));
+		aux5 = n % 100000;
+		aux5 = aux5 / 1e3;
+		_putchar('0' + (aux5 / 10));
+		aux6 = n % 10000;
+		print_number_4_digits(aux6);
 }
