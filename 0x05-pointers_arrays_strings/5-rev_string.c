feb734946a370b0c21f3d7057b04e2e7b8eebe50
Juank <juan@pop-os.localdomain>
2020-02-18 14:50:47 -0500

    function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+ * rev_string - Function that reverses a string
+ * @s: String for reverses
+ **/
+void rev_string(char *s)
+{
+		short i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	char aux[i];
+
+	i -= 1;
+	for (j = 0; j < i + 1; i--, j++)
+	{
+		aux[j] = s[j];
+		s[j] = s[i];
+		s[i] = aux[j];
+	}
+}
