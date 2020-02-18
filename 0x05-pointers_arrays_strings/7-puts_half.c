59a1ad88e08f238571e58f7f26a44653651fea0e
Juank <juan@pop-os.localdomain>
2020-02-18 18:21:07 -0500

    function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * puts_half - function that prints half of a string
+ * @@str: string pointer
+ * @s: string pointeri
+ * Return: length of a string
+ */
+int _strlen(char *s);
+void puts_half(char *str)
+{
+	short i, len;
+
+	len = _strlen(str);
+	if (len % 2 != 0)
+		len = (len - 1) / 2;
+	else
+		len /= 2;
+	for (i = len ; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
