3283f719706c96ea5ba1eec38c84092e08c1e51e
Juank <juan@pop-os.localdomain>
2020-02-25 13:24:55 -0500

    function that copies memory area

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the beginning of the located substring, or NULL if the
+ * substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int aux;
+	int j;
+	int i = 0;
+	int len_nee = _strlen(needle);
+
+	while (haystack[i])
+	{
+		aux = j = 0;
+		if (haystack[i] == needle[0])
+		{
+			aux = i + 1;
+			while (haystack[aux - 1] == needle[j])
+			{
+				if (j == len_nee)
+				{
+					return (haystack + i);
+				}
+				j++;
+				aux++;
+			}
+		}
+		i++;
+	}
+	return ('\0');
+}
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
