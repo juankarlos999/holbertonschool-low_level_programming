6181388832bcc55dd4bbb6ca3b3b223be31043d7
Juank <juan@pop-os.localdomain>
2020-02-26 00:01:04 -0500

    function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: positions
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j, i;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
