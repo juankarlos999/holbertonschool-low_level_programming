d394f4d885ed5860ebe939fff63add8bf06b086c
Juank <juan@pop-os.localdomain>
2020-02-27 11:36:45 -0500

    function that compares a string if it is palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * is_palindrome - function that compares a string if it is palindrome
+ * @s: string input
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	int size = _strlen(s);
+
+	size -= 1;
+	if (s[0] != s[size])
+		return (0);
+	else if (s[0] == s[size])
+		return (comp_string(s, size, 0));
+	return (0);
+}
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
+/**
+ * comp_string - compare a string from its opposites
+ * @string: input
+ * @size: string size
+ * @aux: counter
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
+int comp_string(char *string, int size, int aux)
+{
+	if (string[aux] == string[size])
+	{
+		if (size > aux)
+			return (1);
+		return (comp_string(string, (size - 1), (aux + 1)));
+	}
+	else
+		return (0);
+}
