59106c15da7a7db6f9bc50ba20982e1feab34b2a
Juank <juan@pop-os.localdomain>
2020-03-09 16:53:33 -0500

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,23 +14,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = _strdup(name);
-		if (new_dog->owner == 0)
-	{
-		free(new_dog);
-		return (0);
-	}
 	new_dog->age = age;
 	new_dog->owner = _strdup(owner);
-	if (new_dog->owner == 0)
+	if (new_dog->owner == NULL)
 	{
-		free(new_dog);
 		free(new_dog->name);
-		return (0);
+		free(new_dog);
+		return (NULL);
 	}
 	return (new_dog);
 }
