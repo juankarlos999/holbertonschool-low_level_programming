68a24354c3e67ec2cfbeb29fd0bcecc1e906e14e
Juank <juan@pop-os.localdomain>
2020-03-09 14:31:52 -0500

    [update]Library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - struct of data type dog
  * @name: name dog
@@ -17,4 +18,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
 #endif
