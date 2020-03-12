7690aed2f83aba60d497f8034a649df44675419e
Juank <juan@pop-os.localdomain>
2020-03-11 23:30:28 -0500

    program that prints the opcodes
    of its own main functioat prints the opcodes of its own
    main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
+/**
+ *main - program prints opcodes
+ *@argc:Number of arguments
+ *@argv: Arguments
+ *
+ *Return: Sucess (0)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	unsigned char *p;
+	int (*mn)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	p = (unsigned char *)mn;
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < bytes - 1; i++)
+	{
+		printf("%02x ", *p);
+		p++;
+	}
+		printf("%02x\n", *p);
+	return (0);
+}
