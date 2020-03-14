b5e486e6660e2b8c5d6425ac16ead5d1b5a33534
Juank <juan@pop-os.localdomain>
2020-03-13 22:18:52 -0500

    [update]:new functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,17 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
 #include <stdarg.h>
+typedef struct opc
+{
+	char *array;
+	void (*function_pointer)(va_list argv);
+} input;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list argv);
+void print_integer(va_list argv);
+void print_float(va_list argv);
+void print_string(va_list argv);
 #endif
