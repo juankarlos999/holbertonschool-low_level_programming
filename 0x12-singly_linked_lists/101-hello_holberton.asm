14fb2be2793cbef8dd2bb544c384f42a53681f2d
juankarlos999 <juankarlos999@github.com>
2020-03-21 18:09:31 -0500

    Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,15 +3,15 @@ section .rodata
     name:   db 'Holberton', 0
 
 section .text
-        global main
-        extern printf
-    main:
-        ; printf(format, name)
-        mov rdi, format
-        mov rsi, name
-        ; no XMM registers
-        mov rax, 0
-        call printf
-        ; return 0
-        mov rax, 0
-        ret
+		global main
+		extern printf
+	main:
+		; printf(format, name)
+		mov rdi, format
+		mov rsi, name
+		; no XMM registers
+		mov rax, 0
+		call printf
+		; return 0
+		mov rax, 0
+		ret
