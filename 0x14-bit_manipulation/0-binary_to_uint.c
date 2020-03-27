2a26c19abd6aba404d239c84cf0665ba6710a952
juankarlos999 <juankarlos999@github.com>
2020-03-26 22:00:30 -0500

    function that converts a binary number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num_dec = 0;
-	int i, j, len = 0, aux = 1, table_binary[100];
+	int i, len = 0, aux = 1;
 
 	if (b == NULL)
 		return (0);
@@ -17,13 +17,13 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		len++;
 	}
-	table_binary[0] = 1;
 	if (len == 1 && b[len] == '1')
 		return (num_dec += 1);
-	for (i = 1; i <= len; i++)
-		table_binary[i] = (aux *= 2);
-	for (i = 0, j = len - 1; len >= 0; i++, len--, j--)
+	for (i = len - 1; i >= 0; i--)
+	{
 		if (b[i] == '1')
-			num_dec += (unsigned int)table_binary[j];
+			num_dec += aux;
+		aux *= 2;
+	}
 	return (num_dec);
 }
