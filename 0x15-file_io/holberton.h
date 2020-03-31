8d2f5896f5e2458413036afda0f69fa924b41662
juankarlos999 <juankarlos999@github.com>
2020-03-31 18:02:44 -0500

    [update]: quit libraries

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,10 +1,8 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
