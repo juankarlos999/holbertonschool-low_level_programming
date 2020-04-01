542d2a05aab5631cf03ef2818928e18aa2fbd6eb
juankarlos999 <juankarlos999@github.com>
2020-03-31 20:18:44 -0500

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,8 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-	write(fd, text_content, _strlen(text_content));
+	if (text_content != NULL)
+		write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
