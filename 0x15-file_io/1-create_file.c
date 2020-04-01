b3a734f3ff22df90bbaa3f112b344870977f56ae
juankarlos999 <juankarlos999@github.com>
2020-03-31 20:12:45 -0500

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,18 +8,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	unsigned int _write;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
-		return (0);
+	{
+		close(fd);
+		return (-1);
+	}
 	if (text_content == NULL)
-		return (1);
-	_write = write(fd, text_content, _strlen(text_content));
-	if ((int)_write == -1)
-		write(2, "fails", 5);
+	{
+		close(fd);
+		return (-1);
+	}
+	write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
