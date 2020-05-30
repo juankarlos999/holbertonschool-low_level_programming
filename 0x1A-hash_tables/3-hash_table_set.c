0d8da14f63437f6d7c20157f9d2445713811908b
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-29 23:04:06 -0500

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (new_node != NULL)
 	{
-		if (!strcmp(new_node->key, key))
+		if (strcmp(new_node->key, key) == 0)
 		{
 			free(new_node->value), new_node->value = copy_value;
 			return (1);
