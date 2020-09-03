a1547d70f3ca17f7ce4ed5079fe45b606b0d2c8c
juankarlos999 <juan_karlos_999@hotmail.com>
2020-09-02 19:31:46 -0500

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,6 @@
  * @array: pointer to the first element of the array to print
  * @min: left boundary
  * @max: right boundary
- *
  * Return: No Return
  */
 void print_array(int *array, int min, int max)
@@ -24,7 +23,6 @@ void print_array(int *array, int min, int max)
  * @array: pointer to the first element of the array to search in
  * @size:  number of elements in array
  * @value: value to search for
- *
  * Return: Index where value is located or -1
  */
 int binary_search(int *array, size_t size, int value)
